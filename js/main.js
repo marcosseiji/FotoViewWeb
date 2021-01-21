dcbeea9b1a59988ff4ddbecb4c6b147a4c070a64
Marcos Henrique Seiji <seiji.marcos@gmail.com>
2021-01-21 15:42:25 -0300

    Melhorias -  Abrir e fechar camera

--- js/main.js
+++ js/main.js
@@ -1,47 +1,21 @@
 'use strict';
 
+const openCamera = document.querySelector("#openCamera");
+const closeCamera = document.querySelector("#closeCamera");
+const screenshotButton = document.querySelector("#screenshot-button");
+const videoSource = document.querySelector("#videoSource");
+const img = document.querySelector("#screenshot img");
+const video = document.querySelector("#video");
+const canvas = document.createElement("canvas");
+
+openCamera.hidden = false;
+closeCamera.hidden = true;
+screenshotButton.hidden = true;
+screenshotButton.hidden = true;
+videoSource.hidden = true;
+
 var videoElement = document.querySelector('video');
 var videoSelect = document.querySelector('select#videoSource');
-
-videoSelect.onchange = getStream;
-
-getStream().then(getDevices).then(gotDevices);
-
-function getDevices() {
-    return navigator.mediaDevices.enumerateDevices();
-}
-
-function gotDevices(deviceInfos) {
-    window.deviceInfos = deviceInfos; // make available to console
-    console.log('Available input and output devices:', deviceInfos);
-    for (const deviceInfo of deviceInfos) {
-        const option = document.createElement('option');
-        option.value = deviceInfo.deviceId;
-        if (deviceInfo.kind === 'videoinput') {
-            option.text = deviceInfo.label || `Camera ${videoSelect.length + 1}`;
-            videoSelect.appendChild(option);
-        }
-    }
-}
-
-function getStream() {
-    if (window.stream) {
-        window.stream.getTracks().forEach(track => {
-            track.stop();
-        });
-    }
-    const videoSource = videoSelect.value;
-    const constraints = {
-
-        video: {
-            deviceId: videoSource ? {
-                exact: videoSource
-            } : undefined
-        }
-    };
-    return navigator.mediaDevices.getUserMedia(constraints).
-    then(gotStream).catch(handleError);
-}
 
 function gotStream(stream) {
     window.stream = stream;
@@ -54,10 +28,74 @@ function handleError(error) {
     console.error('Error: ', error);
 }
 
-const screenshotButton = document.querySelector("#screenshot-button");
-const img = document.querySelector("#screenshot img");
-const video = document.querySelector("#video");
-const canvas = document.createElement("canvas");
+openCamera.onclick = function () {
+
+    openCamera.hidden = true;
+    closeCamera.hidden = false;
+    screenshotButton.hidden = false;
+    videoSource.hidden = false;
+
+    videoSelect.onchange = getStream;
+
+    getStream().then(getDevices).then(gotDevices);
+
+    function getStream() {
+        if (window.stream) {
+            window.stream.getTracks().forEach(track => {
+                track.stop();
+            });
+        }
+        const videoSource = videoSelect.value;
+        const constraints = {
+
+            video: {
+                deviceId: videoSource ? {
+                    exact: videoSource
+                } : undefined
+            }
+        };
+        return navigator.mediaDevices.getUserMedia(constraints).
+        then(gotStream).catch(handleError);
+    }
+
+    function getDevices() {
+        return navigator.mediaDevices.enumerateDevices();
+    }
+
+    function gotDevices(deviceInfos) {
+        window.deviceInfos = deviceInfos; // make available to console
+        console.log('Available input and output devices:', deviceInfos);
+        for (const deviceInfo of deviceInfos) {
+            const option = document.createElement('option');
+            option.value = deviceInfo.deviceId;
+            if (deviceInfo.kind === 'videoinput') {
+                option.text = deviceInfo.label || `Camera ${videoSelect.length + 1}`;
+                videoSelect.appendChild(option);
+            }
+        }
+    }
+}
+
+closeCamera.onclick = function () {
+
+    openCamera.hidden = false;
+    closeCamera.hidden = true;
+    screenshotButton.hidden = true;
+    videoSource.hidden = true;
+
+    video.pause();
+    video.currentTime = 0;
+
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+
+        navigator.mediaDevices.getUserMedia({
+            video: true
+        }).then(function (stream) {
+            video.srcObject = null;
+        });
+    }
+
+}
 
 screenshotButton.onclick = video.onclick = function () {
 
